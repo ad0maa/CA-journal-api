f6a02282550b3ad06b5b006fde3911ef688811ac
ad0maa <adam.tunchay@me.com>
2023-01-10 14:15:20 +1100

    modularise routes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import {EntryModel, CategoryModel } from './db.js'
+import { CategoryModel } from "./db.js";
+import entryRoutes from "./routes/entry_routes.js";
 
 const app = express();
 const port = 4001;
@@ -12,94 +13,8 @@ app.get("/categories", async (req, res) =>
   res.status(200).send(await CategoryModel.find())
 );
 
-app.get("/entries", async (req, res) => res.send(await EntryModel.find()));
 
-app.get("/entries/food", async (req, res) =>
-  res.send(await EntryModel.find({ category: "Food" }))
-);
-
-// POST new entry to the database
-app.post("/entries", async (req, res) => {
-  try {
-    // Create a new entry object with values passed in from the request
-    const { category, content } = req.body;
-    const newEntry = { category, content };
-    // Push new entry object into entries array
-    const insertedEntry = await EntryModel.create(newEntry);
-    // Send the new entry with 201 status code
-    res.status(201).send(insertedEntry);
-  } catch (err) {
-    res.status(500).send({ error: err.message });
-  }
-});
-
-// GET a single entry from the database by id
-app.get("/entries/:id", async (req, res) => {
-  try {
-    const entry = await EntryModel.findById(req.params.id);
-    if (entry) {
-      res.send(entry);
-    } else {
-      res.status(404).send({ error: "Entry not found" });
-    }
-  } catch (err) {
-    res.status(500).send({ error: err.message });
-  }
-});
-
-// PUT Update an entry
-
-app.put("/entries/:id", async (req, res) => {
-  const { category, content } = req.body;
-  const newEntry = { category, content };
-
-  try {
-    const entry = await EntryModel.findByIdAndUpdate(req.params.id, newEntry, {
-      returnDocument: "after",
-    });
-    if (entry) {
-      res.send(entry);
-    } else {
-      res.status(404).send({ error: "Entry not found" });
-    }
-  } catch (err) {
-    res.status(500).send({ error: err.message });
-  }
-});
-
-// Delete an entry
-
-app.delete("/entries/:id", async (req, res) => {
-  try {
-    const entry = await EntryModel.findByIdAndDelete(req.params.id);
-    if (entry) {
-      res.sendStatus(204);
-    } else {
-      res.status(404).send({ error: "Entry not found" });
-    }
-  } catch (err) {
-    res.status(500).send({ error: err.message });
-  }
-});
-
-// Insert a new entry into an array
-// app.post("/entries", (req, res) => {
-//   // Create a new entry object with values passed in from the request
-//   const { category, content } = req.body;
-//   const newEntry = { category, content };
-//   // Push new entry object into entries array
-//   entries.push(newEntry);
-//   // Send the new entry with 201 status code
-//   res.status(201).send(newEntry);
-// });
-
-// app.get("/entries/:id", (req, res) => {
-//   const entry = entries[req.params.id];
-//   if (entry) {
-//     res.send(entry);
-//   } else {
-//     res.status(404).send({ error: "Entry not found" });
-//   }
-// });
+// Prefixing entries with URI 
+app.use('/entries', entryRoutes);
 
 app.listen(port, () => console.log(`App running @ http//localhost:${port}`));
---
+++ routes/entry_routes.js
@@ -0,0 +1,88 @@
+import express from 'express'
+import { EntryModel } from '../db.js'
+
+const router = express.Router()
+
+
+
+router.get("/", async (req, res) =>
+  res.send(
+    await EntryModel.find().populate({
+      path: "category",
+      select: ["name"],
+    })
+  )
+);
+
+router.get("/food", async (req, res) =>
+  res.send(await EntryModel.find({ category: "Food" }))
+);
+
+// POST new entry to the database
+router.post("/", async (req, res) => {
+  try {
+    // Create a new entry object with values passed in from the request
+    const { category, content } = req.body;
+    const newEntry = { category, content };
+    // Push new entry object into entries array
+    const insertedEntry = await EntryModel.create(newEntry);
+    // Send the new entry with 201 status code
+    res.status(201).send(insertedEntry);
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+});
+
+// GET a single entry from the database by id
+router.get("/:id", async (req, res) => {
+  try {
+    const entry = await EntryModel.findById(req.params.id).populate({
+      path: "category",
+      select: ["name"],
+    });
+    if (entry) {
+      res.send(entry);
+    } else {
+      res.status(404).send({ error: "Entry not found" });
+    }
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+});
+
+// PUT Update an entry
+
+router.put("/:id", async (req, res) => {
+  const { category, content } = req.body;
+  const newEntry = { category, content };
+
+  try {
+    const entry = await EntryModel.findByIdAndUpdate(req.params.id, newEntry, {
+      returnDocument: "after",
+    });
+    if (entry) {
+      res.send(entry);
+    } else {
+      res.status(404).send({ error: "Entry not found" });
+    }
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+});
+
+// Delete an entry
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const entry = await EntryModel.findByIdAndDelete(req.params.id);
+    if (entry) {
+      res.sendStatus(204);
+    } else {
+      res.status(404).send({ error: "Entry not found" });
+    }
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+});
+
+export default router
