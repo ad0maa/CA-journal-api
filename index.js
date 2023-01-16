cbfbb0eb73bec6fb36509adcbc3dbc93f25a1ee1
ad0maa <adam.tunchay@me.com>
2023-01-16 15:10:41 +1100

    port updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import app from './app.js'
 
-const port = process.envPORT || 4001;
+const port = process.env.PORT || 4001;
 
 app.listen(port, () => console.log(`App running @ http//localhost:${port}`));
  
